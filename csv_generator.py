97c71f8fb35e0897c2821a519054d837f5955c66
Felipe Netto <flpncc@gmail.com>
2017-12-21 22:30:31 -0300

    Parametrizando nome de CSV gerado para tornar funcao mais generica

--- csv_generator.py
+++ csv_generator.py
@@ -9,13 +9,13 @@ def merge_csv(csv_a, csv_b, columns):
     return csv_a.merge(csv_b, on=columns)
 
 
-def generate_file_from_json_and_csv(json_path, csv_path, columns):
+def generate_file_from_json_and_csv(json_path, csv_path, file_name, columns):
     csv_a = json_to_csv(json_path)
     csv_b = pd.read_csv(csv_path)
 
-    database = merge_csv(csv_a, csv_b, columns)
-    database.to_csv('database.csv', index=False)
+    file = merge_csv(csv_a, csv_b, columns)
+    file.to_csv(file_name, index=False)
 
 
 if __name__ == '__main__':
-    generate_file_from_json_and_csv('pytan_series.json', 'via_lactea_series.csv', ['name', 'year'])
+    generate_file_from_json_and_csv('pytan_series.json', 'via_lactea_series.csv', 'database.csv', ['name', 'year'])
