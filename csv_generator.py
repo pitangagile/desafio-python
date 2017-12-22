0bd3c752a6bba00baefc2327a8aa72f800eb7517
Felipe Netto <flpncc@gmail.com>
2017-12-21 22:42:07 -0300

    Refatorando código

--- csv_generator.py
+++ csv_generator.py
@@ -1,19 +1,11 @@
 import pandas as pd
 
 
-def json_to_csv(json_path):
-    return pd.read_json(json_path)
-
-
-def merge_csv(csv_a, csv_b, columns):
-    return csv_a.merge(csv_b, on=columns)
-
-
 def generate_file_from_json_and_csv(json_path, csv_path, file_name, columns):
-    csv_a = json_to_csv(json_path)
+    csv_a = pd.read_json(json_path)
     csv_b = pd.read_csv(csv_path)
 
-    file = merge_csv(csv_a, csv_b, columns)
+    file = csv_a.merge(csv_b, on=columns)
     file.to_csv(file_name, index=False)
 
 
