1b137b6b31fbd380d72e52eecb3ecb78b491bbd1
Filipe Mei <peticormei@gmail.com>
2018-01-10 08:44:58 -0300

    Implementação do merge entre o arquivo json e csv sem usar o pandas

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+import csv
+
+json_file = open("pytan_series.json")
+list_json_pytan = json.load(json_file)
+
+database = []
+
+with open("via_lactea_series.csv") as csv_file:
+    fieldnames = csv_file.readline().split(",")
+    fieldnames = [x.rstrip() for x in fieldnames]
+    list_json_lactea = csv.DictReader(csv_file, fieldnames)
+
+    for lactea, pytan in zip(list_json_lactea, list_json_pytan):
+        database.append({**lactea, **pytan})
+
+with open("database.csv", "w") as csv_out:
+    fieldnames = [x for x in database[0].keys()]
+    writer = csv.DictWriter(csv_out, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerows(database)
